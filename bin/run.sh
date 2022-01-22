9ab1e69d108b4d56b98c170e3351f5dae26b2f30
Javier Sánchez <javiersanp@gmail.com>
2022-01-22 18:36:52 +0000

    Actualiza run.sh a python3

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-python3 -V > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    python3 /opt/CatAtom2Osm/catatom2osm $*
-else
-    python2 /opt/CatAtom2Osm/catatom2osm $*
-fi
+python3 /opt/CatAtom2Osm/catatom2osm $*
